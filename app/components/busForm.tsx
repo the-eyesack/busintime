387eca9808ecbd78cab47c301f2902afe026ad91
Isaac Wong <47isaacw@gmail.com>
2024-04-26 00:23:21 -0400

    update sbs translation

--- app/components/busForm.tsx
+++ app/components/busForm.tsx
@@ -12,14 +12,14 @@ export default function BusForm() {
 
 	function translateSBS(route) {
 		if(route == "") {return 'M1'}
-		return route.replace('SBS', '+').toUpperCase()
+		return route.replace(' ', '').replace('SBS', '%2B').toUpperCase()
 	}
 
 	return (
 		<section className={'mx-4 inline-block'}>
 			<form action={`/${(translateSBS(busName))}`} className={'flex'}>
 				<label htmlFor="busName"></label>
-				<input placeholder={'Search for a bus here...'} className={"border-2 p-2 rounded-md w-48"} value={busName.toUpperCase()} onChange={(e)=> {setBusName(e.target.value)}}/>
+				<input placeholder={'Search for a bus here...'} className={"border-2 p-2 rounded-md w-48"} value={busName.toUpperCase()} onChange={(e)=> {setBusName(translateSBS(e.target.value))}}/>
 				<button className={'border-2 rounded-md p-2 mx-2'}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
 			</form>
 
